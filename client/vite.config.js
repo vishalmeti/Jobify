8e154ccb96092b1ec0862ab0110b7eb2cc1d3164
vishalmeti <vishalmeti9@gmail.com>
2024-05-05 22:37:09 +0530

    Revert "vite reciever setting change"
    This reverts commit 97c6bd60c5fbe521667a4bccf3125c20b4363e7c.

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: '/api',
+        target: 'http://localhost:5100/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
